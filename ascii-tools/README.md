c8060c6b19d64e95d448ca996a1368ad8acce4bd
Ali Clubb <ali@clubb.org.uk>
2012-05-18 12:28:00 +0100

    Updated README.md and added html conversion features to ascii-tools.c

--- ascii-tools/README.md
+++ ascii-tools/README.md
@@ -3,3 +3,7 @@ ascii-tools
 
 	This is a collection of tools I am developing to deal with ASCII related conversion.
 	So far it does strings to ASCII values (-ascii) and ASCII values to strings (-char) :)
+	
+	NOTE: The char to html function works for most characters, but breaks upon characters such as é
+	being entered. Also, html to ascii and html to char are currently not supported. I will add support
+	when I find the time.
--- ascii-tools/ascii-tools.c
+++ ascii-tools/ascii-tools.c
@@ -3,25 +3,49 @@
 #include <string.h>
 int main (int argc, char const *argv[])
 {
-	int i, c;
 	if(argc == 1)
-		printf("Usage: ascii-tools [options]\nOptions:\n-ascii\n-char\n");
-	else if(strcmp(argv[1], "-ascii") == 0)
+		printf("Usage: ascii-tools [inform] [outform]\nOptions:\nAny combination of the following:\n-ascii\n-char\n-html\n");
+	else if((strcmp(argv[1], "-char") == 0) && (strcmp(argv[2], "-ascii") == 0))
 	{
-		/* Small character to ASCII number converter */
+		/* Character to ASCII */
+		int c;
 		while ((c = getchar()) != EOF)
 			printf("%i ", c);
 	}
-	else if(strcmp(argv[1], "-char") == 0)
+	else if((strcmp(argv[1], "-ascii") == 0) && (strcmp(argv[2], "-char") == 0))
 	{
-		/* Small ASCII number to character converter */
+		/* ASCII to character */
+		int i;
 		char str[1];
 		while ((scanf("%s", str)) != EOF)
 		{
 			i = atoi(str);
 			putchar(i);
 		}
-		putchar('\n');
+	}
+	else if((strcmp(argv[1], "-char") == 0) && (strcmp(argv[2], "-html") == 0))
+	{
+		/* Character to html code */
+		int c;
+		while ((c = getchar()) != EOF)
+		{
+			if(c >= 9 && c <= 255)
+			{
+				printf("&#");
+			}
+			printf("%i ", c);
+		}
+	}
+	else if((strcmp(argv[1], "-ascii") == 0) && (strcmp(argv[2], "-html") == 0))
+	{
+		/* ASCII to html code */
+		int i;
+		char str[1];
+		while (scanf("%s", str))
+		{
+			i = atoi(str);
+			printf("&#%i ", i);
+		}
 	}
 	return 0;
 }
