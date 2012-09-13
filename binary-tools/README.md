bce3f3876a72ea6686bcdb99c689c4401ed097c7
Ali Clubb <ali@clubb.org.uk>
2012-09-13 11:34:36 +0100

    Updated bin2num.c and README.md

--- binary-tools/README.md
+++ binary-tools/README.md
@@ -3,3 +3,5 @@ binary-tools
 
 	This is a collection of tools I am developing to deal with binary related conversion.
 	
+	NOTE: For the moment, convertendian only deals with binary numbers. Hopefully this will be changed
+	in the future.
--- binary-tools/bin2num.c
+++ binary-tools/bin2num.c
@@ -3,28 +3,35 @@
 
 int main (int argc, char const *argv[])
 {
-	int total = 0, ptr = 0, i = 0;
-	char input[10];
+	int total, ptr, i = 0;
+	char input[100];
 	scanf("%s", input);
-	while (input[i] != EOF)
-		++i;
-	while (i >= 0)
+	while (input[i] != '\0')
 	{
-		switch(input[i--])
+		total = 0;
+		ptr = 0;
+		while (input[i] != EOF)
+			++i;
+		while (i >= 0)
 		{
-			case '1' : 
+			switch(input[i--])
 			{
-				total += pow(2, ptr++);
-				break;
+				case '1' : 
+				{
+					total += pow(2, ptr++);
+					break;
+				}
+				case '0' : 
+				{
+					ptr++;
+					break;
+				}
 			}
-			case '0' : 
-			{
-				ptr++;
-				break;
-			}
-			default : break;
 		}
+		printf("%i\n", total);
+		input[0-100] = '\0';
+		i = 0;
+		scanf("%s", input);
 	}
-	printf("%i\n", total);
 	return 0;
 }
